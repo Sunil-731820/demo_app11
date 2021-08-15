4d0611eedaa1f1fbebbe1a0e4b05373c84622873
sunilkumar1170 <sunilkmar1170@gmail.com>
2021-08-15 11:24:26 +0530

    updated widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MaterialApp(
+      title: "Awesome App",
+      home: Center(
+        child: Container(
+          color: Colors.blueAccent,
+          //margin:EdgeInsets.all(30) ,
+          child: Text(
+            "happy independence day to all my friends ",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          //alignment: Alignment(0.0, 0.0),
+          alignment: FractionalOffset(0.2, 0.0),
+          constraints: BoxConstraints(
+              maxWidth: 500.0,
+              maxHeight: 500.0,
+              minWidth: 200.0,
+              minHeight: 200.0),
+        ),
+      ),
+    ));
